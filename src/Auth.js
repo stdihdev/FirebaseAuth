cf5ebaf281d8e35fc321ccc97f0a658a839ab910
stevendinhdev <stevendinhdev@gmail.com>
2018-07-09 19:12:52 +0530

    Add Firebase Sign in with Email and Password :+1:

--- src/Auth.js
+++ src/Auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 const firebase = require('firebase');
 
 // Initialize Firebase
-  var config = {
+  let config = {
     apiKey: "AIzaSyDrFCs8FeprCa7rQoOg_9vEuvkJIB8vUZc",
     authDomain: "authentication-f0e56.firebaseapp.com",
     databaseURL: "https://authentication-f0e56.firebaseio.com",
@@ -13,9 +13,32 @@ const firebase = require('firebase');
   firebase.initializeApp(config);
 
 export default class Auth extends Component{
+  constructor(props){
+    super(props);
+    this.state = {error: ''};
+    this.logIn = this.logIn.bind(this);
+  }
+  logIn(){
+    let email = this.refs.email.value;
+    let password = this.refs.password.value;
+    let loginStatus = firebase.auth().signInWithEmailAndPassword(email, password);
+
+    loginStatus.catch((err) => {
+      this.setState({error: err.message});
+    });
+  }
   render(){
     return (
-      <h1>Authentication</h1>
-    );
+      <div>
+        <p>{this.state.error}</p>
+        <input id="email" ref="email" type="email" placeholder="Email" />
+          <br />
+        <input id="password" ref="password" type="password" placeholder="Password" />
+          <br />
+          <button onClick={this.logIn} type="button">LogIn</button>
+          <button type="button">SignUp</button>
+          <button type="button">LogOut</button>
+        </div>
+  );
   }
 }
